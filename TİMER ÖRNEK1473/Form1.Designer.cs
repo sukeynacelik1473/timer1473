723623d1f1550c8aa33eea870ca5e0272a477709
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2024-01-09 11:37:44 +0300

    Add files via upload

---
+++ TİMER ÖRNEK1473/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TİMER_ÖRNEK1473
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.lblSayac = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBasla.Location = new System.Drawing.Point(130, 99);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(246, 85);
+            this.btnBasla.TabIndex = 0;
+            this.btnBasla.Text = "BAŞLA";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDurdur.Location = new System.Drawing.Point(400, 99);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(246, 85);
+            this.btnDurdur.TabIndex = 1;
+            this.btnDurdur.Text = "DURDUR";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // lblSayac
+            // 
+            this.lblSayac.AutoSize = true;
+            this.lblSayac.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayac.Location = new System.Drawing.Point(373, 225);
+            this.lblSayac.Name = "lblSayac";
+            this.lblSayac.Size = new System.Drawing.Size(30, 31);
+            this.lblSayac.TabIndex = 2;
+            this.lblSayac.Text = "0";
+            this.lblSayac.Click += new System.EventHandler(this.lblSayac_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSayac);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.btnBasla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Label lblSayac;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TİMER ÖRNEK1473/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TİMER_ÖRNEK1473
+{
+    
+    public partial class Form1 : Form
+    {
+        int sayac = 10;
+        DateTime zaman2 = new DateTime(2024, 01, 09, 11, 40, 0);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            this.BackColor = Color.Yellow;
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void lblSayac_Click(object sender, EventArgs e)
+        {
+            sayac++;
+            lblSayac.Text = sayac.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            TimeSpan fark = zaman2 - DateTime.Now;
+
+            lblSayac.Text=$"{fark.Minutes} : {fark.Seconds}";
+        }
+    }
+}
